351ab2394ff353c151bb8f6fcc072aeb15527370
Samie Smilz <samiesmilz@gmail.com>
2023-09-02 01:51:46 +0800

    Updated script.js

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function countDown(arg) {
 }
 
 countDown(4);
+
+/*## **randomGame**
+Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop the timer and console.log the number of tries it took before we found a number greater than .75. */
+
+function randomGame() {
+  let counter = 1;
+  const id = setInterval(function () {
+    const randomNumber = Math.random();
+    console.log(`Try ${counter}: ${randomNumber}`);
+
+    if (randomNumber > 0.75) {
+      clearInterval(id);
+      console.log(
+        `It took ${counter} tries to find the number greater than .75`
+      );
+    }
+    counter = counter + 1;
+  }, 1000);
+}
+
+setTimeout(randomGame, 5000);
