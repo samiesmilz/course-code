435a8c1b12b7599978b239b8f93668e749e0f469
Samie Smilz <samiesmilz@gmail.com>
2023-09-06 15:35:05 +0800

    Updated Condition in script.js
    Improve the conditional statement in the countdown function to handle negative input

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ Once the value is 0 it should log “DONE!” and stop. */
 function countDown(arg) {
   const id = setInterval(function () {
     arg = arg - 1;
-    if (arg !== 0) {
+    if (arg > 0) {
       console.log(arg);
     } else {
       console.log("DONE!");
@@ -17,7 +17,9 @@ function countDown(arg) {
 countDown(4);
 
 /*## **randomGame**
-Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop the timer and console.log the number of tries it took before we found a number greater than .75. */
+Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds 
+and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, 
+stop the timer and console.log the number of tries it took before we found a number greater than .75. */
 
 function randomGame() {
   let counter = 1;
